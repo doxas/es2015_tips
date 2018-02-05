a23738cfecf25ad9da860d6d8143834deb5ecc28
sugimoto-m <masahiro-s@digirea.com>
2018-02-05 13:47:09 +0900

    fix general sample

--- general/js/script.js
+++ general/js/script.js
@@ -1,3 +1,15 @@
 
+// es5 style class
+let MyFunc = function(){};
+MyFunc.prototype.print = function(){
+	console.log(this);
+};
+
+window.addEventListener('load', function(){
+	let f = new MyFunc();
+	// click event に MyFunc.print を登録
+	let body = window.document.body;
+	body.addEventListener('click', f.print, false);
+}, false);
 
 
