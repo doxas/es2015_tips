43e2480170e62aa922ce8ec967a55530dc94862a
sugimoto-m <masahiro-s@digirea.com>
2018-02-05 13:58:25 +0900

    update webpack sample

--- webpack/src/lib.js
+++ webpack/src/lib.js
@@ -3,10 +3,32 @@ export default class Utilities {
 	/**
 	 * format of number
 	 * @param {number} number - integer
-	 * @param {number} [length] - default length is 2
+	 * @param {number} [length=2] - format string length
 	 */
 	static zeroPadding(number, length = 2){
 		return (new Array(length).join('0') + number).substr(-length);
+	}
+	/**
+	 * format of date (year, month, date)
+	 * @param {Date} date - date instance
+	 * @param {string} [delimiter='/'] - string of delimiter
+	 */
+	static dateFormat(date, delimiter = '/'){
+		let y = date.getFullYear();
+		let m = Utilities.zeroPadding(date.getMonth() + 1, 2);
+		let d = Utilities.zeroPadding(date.getDate(), 2);
+		return y + delimiter + m + delimiter + d;
+	}
+	/**
+	 * format of time (hours, minutes, seconds)
+	 * @param {Date} date - date instance
+	 * @param {string} [delimiter=':'] - string of delimiter
+	 */
+	static timeFormat(date, delimiter = ':'){
+		let h = Utilities.zeroPadding(date.getHours(), 2);
+		let i = Utilities.zeroPadding(date.getMinutes(), 2);
+		let s = Utilities.zeroPadding(date.getSeconds(), 2);
+		return h + delimiter + i + delimiter + s;
 	}
 }
 
--- webpack/src/main.js
+++ webpack/src/main.js
@@ -3,3 +3,7 @@ import util from './lib.js';
 
 console.log(util.zeroPadding(99, 3));
 
+let d = new Date();
+
+console.log(util.dateFormat(d) + ' ' + util.timeFormat(d));
+
